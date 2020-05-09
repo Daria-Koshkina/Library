b2576428cf7d3bcb2a4f9bf20b0af367e68b182d
Daria-Koshkina <daria.koshkina@nure.ua>
2020-05-09 16:51:28 +0300

    Added changes in form empty book

--- LibraryApp/EmptyBook.Designer.cs
+++ LibraryApp/EmptyBook.Designer.cs
@@ -38,6 +38,11 @@
             this.press_like = new System.Windows.Forms.CheckBox();
             this.delete_book = new System.Windows.Forms.Button();
             this.number_of_likes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.year)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,7 +62,7 @@
             // 
             // year
             // 
-            this.year.Location = new System.Drawing.Point(42, 143);
+            this.year.Location = new System.Drawing.Point(42, 147);
             this.year.Maximum = new decimal(new int[] {
             2021,
             0,
@@ -97,7 +102,7 @@
             // genres
             // 
             this.genres.FormattingEnabled = true;
-            this.genres.Location = new System.Drawing.Point(42, 189);
+            this.genres.Location = new System.Drawing.Point(42, 197);
             this.genres.Name = "genres";
             this.genres.Size = new System.Drawing.Size(140, 21);
             this.genres.Sorted = true;
@@ -141,11 +146,61 @@
             this.number_of_likes.TabIndex = 11;
             this.number_of_likes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Author";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Year";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Genre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Annotation";
+            // 
             // EmptyBook
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.number_of_likes);
             this.Controls.Add(this.delete_book);
             this.Controls.Add(this.press_like);
@@ -176,5 +231,10 @@
         private System.Windows.Forms.CheckBox press_like;
         private System.Windows.Forms.Button delete_book;
         private System.Windows.Forms.Label number_of_likes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
